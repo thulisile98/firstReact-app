84ed8b51e2c411f57b73dd49215bd646cd1063fb
Angel Mashego <thulisile.ma98@gmail.com>
2023-07-25 15:52:25 +0200

    React-app

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+function Message(){
+    ///JSX Javascript XML syntax//
+    var name;
+    if(name)
+    {
+return <h1>Hello {name}</h1>;
+    }
+    return  <h1>Hello world</h1>;
+}
+
+export default Message;
